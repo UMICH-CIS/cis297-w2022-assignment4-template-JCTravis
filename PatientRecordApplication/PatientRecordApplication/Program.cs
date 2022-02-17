a4e6acea486c6527303154b2661562ce539d5a2b
Jacob <jctravis@umich.edu>
2022-02-17 11:50:36 -0500

    added patient class

--- PatientRecordApplication/PatientRecordApplication/Program.cs
+++ PatientRecordApplication/PatientRecordApplication/Program.cs
@@ -6,6 +6,46 @@ using System.Threading.Tasks;
 
 namespace PatientRecordApplication
 {
+
+    class Patient
+    {
+        private int id;
+        private string name;
+        private decimal balance;
+
+        public Patient(int id, string name, decimal balance)
+        {
+            this.id = id;
+            this.name = name;
+            this.balance = balance;
+        }
+
+        public void AddtoFile()
+        {
+            using (StreamWriter writeTo = File.AppendText("PatientFile.txt"))
+            {
+                w.WriteLine("ID : " + this.id + ", Name : " + this.name + ", Balance " + this.balance, writeTo);
+            }
+        }
+
+    }
+
+    class ReadFiles : Patient
+    {
+
+    }
+
+    class DisplayData
+    {
+
+    }
+
+    class DisplayBalance
+    {
+
+    }
+
+
     class Program
     {
         static void Main(string[] args)
